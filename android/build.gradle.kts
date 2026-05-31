7347fc76ed0d6234d6ed499b9451a3c931c3b2c9
renierr <renierr@users.noreply.github.com>
2026-05-31 13:02:16 +0200

    build(android): dynamically match Kotlin jvmTarget to Java targetCompatibility across subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,25 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        val javaVersion = project.extensions.findByName("android")
+            ?.let { it as? com.android.build.gradle.BaseExtension }
+            ?.compileOptions
+            ?.targetCompatibility
+        
+        compilerOptions {
+            if (javaVersion == JavaVersion.VERSION_1_8) {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            } else if (javaVersion == JavaVersion.VERSION_11) {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+            } else {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
