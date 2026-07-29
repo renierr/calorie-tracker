98cae461f72054d2cc02afa1f39e89c54b22e80f
Renier <renierr@users.noreply.github.com>
2026-07-29 19:53:37 +0200

    Update compileSdk to 37 in android/app/build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "de.renier.calorie_tracker"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
